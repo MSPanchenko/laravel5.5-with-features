8b40c9f46f6863e341360532c74d386a0061b7b8
mspanchenko <maxpanchnko@gmail.com>
2017-12-21 23:22:46 +0200

    add databases in config

--- config/database.php
+++ config/database.php
@@ -1,117 +1,92 @@
 <?php
 
+$mysql = [
+    'driver'      => 'mysql',
+    'unix_socket' => env('DB_SOCKET', ''),
+    'charset'     => 'utf8mb4',
+    'collation'   => 'utf8mb4_unicode_ci',
+    'prefix'      => '',
+    'strict'      => true,
+    'engine'      => null,
+];
+
+$pgSql = [
+    'driver'  => 'pgsql',
+    'charset' => 'utf8',
+    'prefix'  => '',
+    'sslmode' => 'prefer',
+];
+
+$mongoDb = [
+    'driver'  => 'mongodb',
+    'options' => [
+        'connectTimeoutMS' => 1000,
+    ],
+];
+
+$sqLite = [
+    'driver' => 'sqlite',
+    'prefix' => '',
+];
+
+$sqlSrv = [
+    'driver'  => 'sqlsrv',
+    'charset' => 'utf8',
+    'prefix'  => '',
+];
+
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Database Connection Name
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for all database work. Of course
-    | you may use many connections at once using the Database library.
-    |
-    */
-
-    'default' => env('DB_CONNECTION', 'mysql'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here are each of the database connections setup for your application.
-    | Of course, examples of configuring each database platform that is
-    | supported by Laravel is shown below to make development simple.
-    |
-    |
-    | All database work in Laravel is done through the PHP PDO facilities
-    | so make sure you have the driver for your particular database of
-    | choice installed on your machine before you begin development.
-    |
-    */
+    'default' => env('DB_CONNECTION', 'defaultMysql'),
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-        ],
+        'defaultMysql' => array_merge($mysql, [
+            'host'     => env('MYSQL_HOS', '127.0.0.1'),
+            'port'     => env('MYSQL_PORT', '3306'),
+            'database' => env('MYSQL_DATABASE', 'test'),
+            'username' => env('MYSQL_USERNAME', 'test'),
+            'password' => env('MYSQL_PASSWORD', 'test'),
+        ]),
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
+        'defaultPgsql' => array_merge($pgSql, [
+            'host'     => env('PGSQL_HOST', '127.0.0.1'),
+            'port'     => env('PGSQL_PORT', '5432'),
+            'database' => env('PGSQL_DATABASE', 'test'),
+            'username' => env('PGSQL_USERNAME', 'test'),
+            'password' => env('PGSQL_PASSWORD', 'test'),
+            'schema'   => env('PGSQL_SCHEMA', 'test'),
+        ]),
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
+        'defaultMongodb' => array_merge($mongoDb, [
+            'host'     => env('MONGODB_HOST', '127.0.0.1'),
+            'port'     => env('MONGODB_PORT', '27017'),
+            'database' => env('MONGODB_DATABASE', 'test'),
+        ]),
 
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-        ],
+        'defaultSqlsrv' => array_merge($sqlSrv, [
+            'host'     => env('SQLSRV_HOST', '127.0.0.1'),
+            'port'     => env('SQLSRV_PORT', '1433'),
+            'database' => env('SQLSRV_DATABASE', 'test'),
+            'username' => env('SQLSRV_USERNAME', 'test'),
+            'password' => env('SQLSRV_PASSWORD', 'test'),
+        ]),
 
+        'defaultSqlite' => array_merge($sqLite, [
+            'database' => env('SQLITE_DATABASE', database_path('database.sqlite')),
+        ]),
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Migration Repository Table
-    |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run in the database.
-    |
-    */
-
     'migrations' => 'migrations',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer set of commands than a typical key-value systems
-    | such as APC or Memcached. Laravel makes it easy to dig right in.
-    |
-    */
 
     'redis' => [
 
         'client' => 'predis',
 
         'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host'     => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
+            'port'     => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
 
